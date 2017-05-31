a3880c2ecda95b8c08d944cbf4586d80af1b9c12
rootventure <theunholydonuts@gmail.com>
2017-05-31 02:10:43 +0000

    Added profiles table

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -21,8 +21,8 @@ class Users::RegistrationsController < Devise::RegistrationsController
   private
     def select_plan
       unless (params[:plan] == '1' || params[:plan] =='2')
-      redirect_to root_url
-      flash[:notice] = "Please select a membership plan to sign up."
+        flash[:notice] = "Please select a membership plan to sign up."
+        redirect_to root_url
       end
     end
 end
