9e6709d83a55a898659a8aef7e761194eeb6b7e4
rootventure <theunholydonuts@gmail.com>
2017-06-03 18:03:21 +0000

    Added user profile forms

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -4,4 +4,26 @@ class ProfilesController < ApplicationController
     # render blank profile details form
     @profile = Profile.new
   end
+  # POST to /users/:user_id/profile
+  def create
+    #get user from the params
+    @user = User.find( params[:user_id] )
+    #this profile will only be linked to the user that is logged in
+    @profile = @user.build_profile( profile_params )
+    #if save successful, display success flash and redirect to homepage
+    if @profile.save
+      flash[:success] = "Profile Updated!"
+      redirect_to root_path
+    else 
+      #if save failed, go display the form again
+      render action: :new
+    end
+  end
+  
+  #whitelisting form fields to be submitted.  New fields cant be added for submission.
+  #private section because this function is only permitted for use within this profile controller file
+  private
+    def profile_params
+      params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
+    end
 end
