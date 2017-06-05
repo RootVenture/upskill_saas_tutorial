d2d4d5f08a52f007afc4bc621c8bda023b337d79
rootventure <theunholydonuts@gmail.com>
2017-06-05 17:18:18 +0000

    Created our index view and action details

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,12 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
 
+  def index
+    @users = User.includes(:profile)
+  end
   # GET to /users/:id
   def show
     #item within params needs to match our URL
     @user = User.find( params[:id] )
-  end
-  
-  def index
-    
   end
 end
